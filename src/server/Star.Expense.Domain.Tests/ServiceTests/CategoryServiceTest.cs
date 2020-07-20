611377f4790cdf5602da5733f1a73aac13eb3504
Tatiana Estrela <tatyyestrela_@hotmail.com>
2020-07-20 14:15:38 -0300

    implementação da interface para serviços

---
+++ src/server/Star.Expense.Domain.Tests/ServiceTests/CategoryServiceTest.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Linq;
+using NUnit.Framework;
+using Star.Expense.Domain.Entities;
+using Star.Expense.Domain.Interfaces;
+using Star.Expense.Domain.Services;
+
+namespace Star.Expense.Domain.Tests.ServiceTests
+{
+    [TestFixture]
+    public class CategoryServiceTest
+    {
+        private ICategoryService _service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _service = new CategoryService();
+        }
+
+        [Test]
+        public void CanCreateNewCategory()
+        {
+            // Arrange
+            var newCategory = new Category()
+            {
+                Name = "Estrela"
+            };
+
+            // Act
+            _service.CreateCategory(newCategory);
+
+            // Asserts
+            Assert.IsTrue(_service.GetCategories().First().Name == "Estrela");
+        }
+        
+        [Test]
+        public void FailWhenAddingSameCategoryName()
+        {
+            // Arrange
+            var newCategory = new Category()
+            {
+                Name = "Estrela"
+            };
+
+            // Act
+            _service.CreateCategory(newCategory);
+
+            // Asserts
+            Assert.Throws<Exception>(() =>
+            {
+                _service.CreateCategory(newCategory);
+            });
+        }
+    }
+}
--- src/server/Star.Expense.Domain/Interfaces/ICategoryService.cs
+++ src/server/Star.Expense.Domain/Interfaces/ICategoryService.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+using Star.Expense.Domain.Entities;
+
 namespace Star.Expense.Domain.Interfaces
 {
     public interface ICategoryService
     {
+        void CreateCategory(Category category);
+        IEnumerable<Category> GetCategories();
         
     }
 }
